d2d1677f0cfabf7acf5b458d8298a72c1cff223d
abayasaye <abay400@gmail.com>
2022-07-24 15:38:57 +0300

    new

--- main.js
+++ main.js
@@ -1,42 +1,57 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Human = /** @class */ (function () {
-    function Human(height, country, user) {
-        this.country = country;
-        this.height = height;
-        console.log(this.user = user);
-    }
-    return Human;
-}());
-var newhuman = new Human(1.70, "indonezia", { firstName: "abay", lastName: "asaye", birth: "29/12/1995" });
-var CarsStore = /** @class */ (function () {
-    function CarsStore(city, country, storeName, carsArray) {
-        this.city = city;
-        this.country = country;
-        this.storeName = storeName;
-        this.carsArray = carsArray;
-    }
-    return CarsStore;
-}());
-console.log();
-var newStore = new CarsStore("los angeles", "usa", "yoshida", [{ brend: "toyota", doorNumbers: 4, wheelsNumber: 4 }, { brend: "truck", doorNumbers: 2, wheelsNumber: 6 }, { brend: "tesla", doorNumbers: 4, wheelsNumber: 4 }]);
-var Garag = /** @class */ (function (_super) {
-    __extends(Garag, _super);
-    function Garag(city, country, storeName, carsArray) {
-        return _super.call(this, city, country, storeName, carsArray) || this;
-    }
-    return Garag;
-}(CarsStore));
+// interface IUser{
+//     firstName:string
+//     lastName:string
+//     birth:string
+// }
+// class Human {
+//     height:number
+//     country:string 
+//     user:IUser
+//     constructor(height:number , country:string , user:IUser){
+//         this.country = country;
+//         this.height =height;
+//         console.log(this.user = user);
+//     }
+// }
+// const newhuman:Human  = new Human(1.70,"indonezia",{firstName:"abay",lastName:"asaye",birth:"29/12/1995" })
+// interface ICar{
+//     brend:string;
+//     doorNumbers:number;
+//     wheelsNumber:number;
+// }
+// class CarsStore {
+//     city:string;
+//     country:string;
+//     storeName:string;
+//     carsArray:ICar[]
+//     constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
+//         this.city = city;
+//         this.country=country;
+//         this.storeName=storeName;
+//         this.carsArray = carsArray;
+//     }
+// }
+// console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
+// class Garag extends CarsStore {
+//     garg:string
+//     ownerName:string
+//     isTzadick:boolean
+//     newCarsArray:ICar[]
+//     constructor(city:string,country:string,storeName:string,carsArray:ICar[],garg:string,ownerName:string,newCarsArray:ICar[]){
+//         super(city,country,storeName,carsArray)
+//         this.city =
+//     }
+// }
+// function genericFunc< T >(gen:T , num:number){
+//     for(i = 0;i<num; i++){
+//         console.log(gen)
+//     }
+// }
+//  genericFunc <string> ("hello world",4 )
+function printGeneric(genString, genNum, someBool) {
+    someBool ?  : ;
+    return genString;
+    var new_Gen;
+    return new_Gen;
+}
+console.log(printGeneric("beautifull world", 2022, true));
--- main.ts
+++ main.ts
@@ -1,45 +1,69 @@
-interface IUser{
-    firstName:string
-    lastName:string
-    birth:string
-}
+// interface IUser{
+//     firstName:string
+//     lastName:string
+//     birth:string
+// }
 
-class Human {
-    height:number
-    country:string 
-    user:IUser
-    constructor(height:number , country:string , user:IUser){
-        this.country = country;
-        this.height =height;
-        console.log(this.user = user);
+// class Human {
+//     height:number
+//     country:string 
+//     user:IUser
+//     constructor(height:number , country:string , user:IUser){
+//         this.country = country;
+//         this.height =height;
+//         console.log(this.user = user);
+//     }
+// }
+// const newhuman:Human  = new Human(1.70,"indonezia",{firstName:"abay",lastName:"asaye",birth:"29/12/1995" })
+
+// interface ICar{
+//     brend:string;
+//     doorNumbers:number;
+//     wheelsNumber:number;
+// }
+
+// class CarsStore {
+//     city:string;
+//     country:string;
+//     storeName:string;
+//     carsArray:ICar[]
+//     constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
+//         this.city = city;
+//         this.country=country;
+//         this.storeName=storeName;
+//         this.carsArray = carsArray;
+//     }
+
+// }
+// console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
+
+// class Garag extends CarsStore {
+//     garg:string
+//     ownerName:string
+//     isTzadick:boolean
+//     newCarsArray:ICar[]
+//     constructor(city:string,country:string,storeName:string,carsArray:ICar[],garg:string,ownerName:string,newCarsArray:ICar[]){
+//         super(city,country,storeName,carsArray)
+//         this.city =
+//     }
+// }
+
+// function genericFunc< T >(gen:T , num:number){
+//     for(i = 0;i<num; i++){
+//         console.log(gen)
+//     }
+// }
+//  genericFunc <string> ("hello world",4 )
+
+
+//  function printGeneric < T ,W>(genString:T,genNum:W,someBool:boolean):T{
+//     someBool?return genString:let new_Gen:T return new_Gen;
+//  }
+//  console.log(printGeneric<string,number>("beautifull world",2022 ,true))
+
+ class User<T> {
+    id:T
+    constructor(id:T){
+        this.id = id;
     }
-}
-const newhuman:Human  = new Human(1.70,"indonezia",{firstName:"abay",lastName:"asaye",birth:"29/12/1995" })
-
-interface ICar{
-    brend:string;
-    doorNumbers:number;
-    wheelsNumber:number;
-}
-
-class CarsStore {
-    city:string;
-    country:string;
-    storeName:string;
-    carsArray:ICar[]
-    constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
-        this.city = city;
-        this.country=country;
-        this.storeName=storeName;
-        this.carsArray = carsArray;
-    }
-
-}
-console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
-
-class Garag extends CarsStore {
-    constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
-        super(city,country,storeName,carsArray)
-        
-    }
-}
+ }
