f7437cb753faad5dcdd3620609645ca4dc15428b
abayasaye <abay400@gmail.com>
2022-07-25 23:37:44 +0300

    interface

--- main.ts
+++ main.ts
@@ -22,31 +22,31 @@
 //     wheelsNumber:number;
 // }
 
-// class CarsStore {
-//     city:string;
-//     country:string;
-//     storeName:string;
-//     carsArray:ICar[]
-//     constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
-//         this.city = city;
-//         this.country=country;
-//         this.storeName=storeName;
-//         this.carsArray = carsArray;
-//     }
+class CarsStore {
+    city:string;
+    country:string;
+    storeName:string;
+    carsArray:ICar[]
+    constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
+        this.city = city;
+        this.country=country;
+        this.storeName=storeName;
+        this.carsArray = carsArray;
+    }
 
-// }
-// console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
+}
+console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
 
-// class Garag extends CarsStore {
-//     garg:string
-//     ownerName:string
-//     isTzadick:boolean
-//     newCarsArray:ICar[]
-//     constructor(city:string,country:string,storeName:string,carsArray:ICar[],garg:string,ownerName:string,newCarsArray:ICar[]){
-//         super(city,country,storeName,carsArray)
-//         this.city =
-//     }
-// }
+class Garag extends CarsStore {
+    garg:string
+    ownerName:string
+    isTzadick:boolean
+    newCarsArray:ICar[]
+    constructor(city:string,country:string,storeName:string,carsArray:ICar[],garg:string,ownerName:string,newCarsArray:ICar[]){
+        super(city,country,storeName,carsArray)
+        this.city =
+    }
+}
 
 // function genericFunc< T >(gen:T , num:number){
 //     for(i = 0;i<num; i++){
@@ -56,10 +56,10 @@
 //  genericFunc <string> ("hello world",4 )
 
 
-//  function printGeneric < T ,W>(genString:T,genNum:W,someBool:boolean):T{
-//     someBool?return genString:let new_Gen:T return new_Gen;
-//  }
-//  console.log(printGeneric<string,number>("beautifull world",2022 ,true))
+ function printGeneric < T ,W>(genString:T,genNum:W,someBool:boolean):T{
+    return someBool? genString:let newgen:T  
+ }
+ console.log(printGeneric<string,number>("beautifull world",2022 ,true))
 
  class User<T> {
     id:T
@@ -67,3 +67,5 @@
         this.id = id;
     }
  }
+ let newUser:User<boolean>= new User<boolean>(true)
+ let newUser21:User<string>= new User<string>("true")
