85bc3aded7d6f67f3663518f3ad93b5f291a562f
abayasaye <abay400@gmail.com>
2022-07-26 18:42:36 +0300

    new

--- main.ts
+++ main.ts
@@ -16,48 +16,64 @@
 // }
 // const newhuman:Human  = new Human(1.70,"indonezia",{firstName:"abay",lastName:"asaye",birth:"29/12/1995" })
 
-// interface ICar{
-//     brend:string;
-//     doorNumbers:number;
-//     wheelsNumber:number;
+// interface ICar {
+//     brend: string;
+//     doorNumbers: number;
+//     wheelsNumber: number;
+// }
+// let newCar: ICar = { brend: "toyota", doorNumbers: 4, wheelsNumber: 4 }
+// let newCar1: ICar = { brend: "truck", doorNumbers: 2, wheelsNumber: 6 }
+// let newCar2: ICar = { brend: "tesla", doorNumbers: 4, wheelsNumber: 4 }
+
+// class CarsStore {
+//     city: string;
+//     country: string;
+//     storeName: string;
+//     carsArray: ICar[]
+//     constructor(city: string, country: string, storeName: string, carsArray: ICar[]) {
+//         this.city = city;
+//         this.country = country;
+//         this.storeName = storeName;
+//         this.carsArray = carsArray;
+//     }
+//     getDtailes() {
+//         return `${this.city}${this.country}${this.storeName}${this.carsArray}`
+//     }
+// }
+// let newStore: CarsStore = new CarsStore("los angeles", "usa", "yoshida", [{ brend: "toyota", doorNumbers: 4, wheelsNumber: 4 },{ brend: "truck", doorNumbers: 2, wheelsNumber: 6 },{ brend: "tesla", doorNumbers: 4, wheelsNumber: 4 }])
+// console.log(newStore)
+
+// class Garag {
+//     garg: string
+//     ownerName: string
+//     isTzadick: boolean
+//     newCarsArray: ICar[]
+//     constructor(garg: string, ownerName: string, isTzadick: boolean, newCarsArray: ICar[]) {
+//         this.garg = garg;
+//         this.ownerName = ownerName;
+//         this.isTzadick = isTzadick;
+//         this.newCarsArray = newCarsArray;
+//     }
+//     getGargDetiels() {
+//         return `${this.garg}${this.ownerName}${this.isTzadick}${this.newCarsArray}`
+
+//     }
 // }
 
-class CarsStore {
-    city:string;
-    country:string;
-    storeName:string;
-    carsArray:ICar[]
-    constructor(city:string,country:string,storeName:string,carsArray:ICar[]){
-        this.city = city;
-        this.country=country;
-        this.storeName=storeName;
-        this.carsArray = carsArray;
-    }
+// let newGarag: Garag = new Garag("canda", "tornto", true, [{ brend: "toyota", doorNumbers: 4, wheelsNumber: 4 },{ brend: "truck", doorNumbers: 2, wheelsNumber: 6 },{ brend: "tesla", doorNumbers: 4, wheelsNumber: 4 }])
+// console.log(newGarag)
 
-}
-console.log(const newStore:CarsStore = new CarsStore("los angeles","usa","yoshida",[{brend:"toyota",doorNumbers:4,wheelsNumber:4},{brend:"truck",doorNumbers:2,wheelsNumber:6},{brend:"tesla",doorNumbers:4,wheelsNumber:4}]))
 
-class Garag extends CarsStore {
-    garg:string
-    ownerName:string
-    isTzadick:boolean
-    newCarsArray:ICar[]
-    constructor(city:string,country:string,storeName:string,carsArray:ICar[],garg:string,ownerName:string,newCarsArray:ICar[]){
-        super(city,country,storeName,carsArray)
-        this.city =
-    }
-}
 
 // function genericFunc< T >(gen:T , num:number){
-//     for(i = 0;i<num; i++){
+//     for(let i = 0;i<num; i++){
 //         console.log(gen)
 //     }
 // }
 //  genericFunc <string> ("hello world",4 )
 
-
  function printGeneric < T ,W>(genString:T,genNum:W,someBool:boolean):T{
-    return someBool? genString:let newgen:T  
+    return someBool? genString:return let newgen:T  
  }
  console.log(printGeneric<string,number>("beautifull world",2022 ,true))
 
